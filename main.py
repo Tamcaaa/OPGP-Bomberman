c2e73013481dc0231a24e89644523c5c27fc2a82
Viliam Pavlovský <viliampavlovsky08@gmail.com>
2026-04-30 10:35:19 +0200

    Remove debug print statement from event handling
    Remove debug print statement for state stack names.

--- main.py
+++ main.py
@@ -51,8 +51,7 @@ class BomberManApp:
 
             if self.state_stack:
                 self.state_stack[-1].handle_events(event)
-            
-            print(f'State Stacks names after event: {[type(s).__name__ for s in self.state_stack]}')
+                
     def draw_text(self, screen: pygame.Surface, text: str, color: pygame.Color | tuple, x: int, y: int):
         text_surface = self.h1_font.render(text, True, color)
         text_rect = text_surface.get_rect()
